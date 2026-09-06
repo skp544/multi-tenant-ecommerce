12496fe43f9440ddc575c8ceb16ff2a6b44263b5
skp544 <shubhamkumarprajapati544@gmail.com>
2026-09-06 14:50:22 +0530

    adding prisma setup to backend folder

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
